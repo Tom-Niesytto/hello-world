ed205b9f339551651afbc29fa64827f377341f60
Tom Niesytto <tom_niesytto@yahoo.com>
2020-03-31 05:12:31 -0500

    adding test_wrapper.pl

---
+++ test_wrapper.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+my %inner;
+
+my @array = ('one','two','three');
+
+my @data = (0,1,2);
+
+my $idx;
+
+my %wrapper = ();
+
+foreach $idx (@data)
+{
+	%inner = ();
+	$inner{$idx} = $array[$idx];
+	my %temp = map {$_ => $inner{$_}} (keys %inner);
+	$wrapper{$idx} = \%temp;
+}
+
+my $inner_idx;
+
+foreach $idx (keys %wrapper)
+{
+	foreach $inner_idx (keys %{$wrapper{$idx}})
+	{
+		my $val = $wrapper{$idx}->{$inner_idx};
+		print STDERR "$val\n";
+	}
+}
+ 
